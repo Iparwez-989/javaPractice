ff128227b118983fcf64131f463f760a854e95b5
Irfan Parwez <iparwez351412@gmail.com>
2024-08-29 19:01:40 +0530

    maxInd

--- maxIndex.java
+++ maxIndex.java
@@ -1,6 +1,6 @@
 public class maxIndex{
     public static void main(String[] args) {
-        int[] arr = {1,2,1,1,3};
+        int[] arr = {1,2,5,3,4,10,6,10};
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < arr.length; i++) {
             if(arr[i]>max){
