c5d713e64c17a22f222736b0f830734530145b59
Irfan Parwez <iparwez351412@gmail.com>
2024-09-13 22:29:29 +0530

    returned subarray

--- a2z/kadanesAlgo.java
+++ a2z/kadanesAlgo.java
@@ -1,6 +1,38 @@
 package a2z;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class kadanesAlgo {
+    static int[] kadanAlgo(int[] arr){
+        int n = arr.length;  //-2,1,-3,4,-1,2,1,-5,4
+        
+        int sum = 0;
+        int maxi = arr[0];
+        int ansStart = -1;
+        int ansEnd = -1;
+        int start = -1;
+        for (int i = 0; i < arr.length; i++) {
+            sum+=arr[i];
+            
+            if(sum>maxi){
+                maxi = Math.max(maxi, sum);
+                ansStart= start;
+                ansEnd=i;
+            }
+            if(sum<0) sum = 0;
+            if(sum == 0) {
+                start = i+1;
+            }
+        }
+        int[] ans = new int[ansEnd-ansStart+1];
+        for(int i =ansStart;i<=ansEnd;i++){
+            ans[i-ansStart]= arr[i];
+        }
+        return ans;
+
+
+    }
     static int kadanes(int[] arr){
         int n = arr.length;
         int sum=0;
@@ -28,9 +60,11 @@ public class kadanesAlgo {
     }
     public static void main(String[] args) {
         int[] arr = {-2,1,-3,4,-1,2,1,-5,4 };
-        int ans = kadanes(arr);
-        System.out.println(ans);
-
+        // int ans = kadanes(arr);
+        // System.out.println(ans);
+        int[] ans = kadanAlgo(arr);
+        System.out.println(Arrays.toString(ans));
+       
         
     }
 }
